b5a03bf6f7552d0dddc5b95e38b68ea5d2788b2a
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-11-11 22:50:20 +0900

    11/11 test

--- app/src/main/java/com/app/getiproject_naranggeti/MainActivity.kt
+++ app/src/main/java/com/app/getiproject_naranggeti/MainActivity.kt
@@ -15,14 +15,14 @@ import com.app.getiproject_naranggeti.ui.theme.GetiProject_naranggetiTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+       setContent {
             GetiProject_naranggetiTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("hello")
                 }
             }
         }
