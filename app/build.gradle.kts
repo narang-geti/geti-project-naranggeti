16fe9c8fdcabd81bcbdc266d36f08ed28ecb9092
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-11-13 09:52:03 +0900

    11/13 firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -59,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.firebase:firebase-ml-vision:24.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -69,4 +71,7 @@ dependencies {
 
     //Navigation
     implementation("androidx.navigation:navigation-compose:2.6.0")
+
+    //firebase_ml
+    implementation ("com.google.firebase:firebase-ml-model-interpreter:22.0.3")
 }
