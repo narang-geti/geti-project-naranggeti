9a573abf3f5239cc2fb3c77f7488f1681fcc9476
jack <jack1264@naver.com>
2023-11-17 11:32:01 +0900

    등급 나오게 만들었음

--- app/src/main/java/com/app/getiproject_naranggeti/DetectScreen.kt
+++ app/src/main/java/com/app/getiproject_naranggeti/DetectScreen.kt
@@ -37,6 +37,10 @@ import androidx.core.graphics.blue
 import androidx.core.graphics.green
 import androidx.core.graphics.red
 import java.io.FileNotFoundException
+import java.lang.Math.exp
+import java.nio.FloatBuffer
+import kotlin.math.exp
+
 
 @Composable
 fun DetectScreen(navController: NavController) {
@@ -46,53 +50,59 @@ fun DetectScreen(navController: NavController) {
     var prediction1 by remember { mutableStateOf("") }
     var label1 by remember { mutableStateOf("") }
     var prediction2 by remember { mutableStateOf("") }
-    var label2 by remember { mutableStateOf("") }
+
     var prediction3 by remember { mutableStateOf("") }
+
+    var label2 by remember { mutableStateOf("") }
+    var predictionNew by remember { mutableStateOf("") }
     val bitmapFromResource1: Bitmap =
-        BitmapFactory.decodeResource(context.resources, R.drawable.paper)
+        BitmapFactory.decodeResource(context.resources, R.drawable.aphone)
     val bitmapFromResource2: Bitmap =
-        BitmapFactory.decodeResource(context.resources, R.drawable.apple)
+        BitmapFactory.decodeResource(context.resources, R.drawable.fphoto)
     val bitmapFromResource3: Bitmap =
-        BitmapFactory.decodeResource(context.resources, R.drawable.plastic)
+        BitmapFactory.decodeResource(context.resources, R.drawable.bphone)
+//    val detectImage by remember { mutableStateOf<Bitmap?>(null)}
 
     var image2 by remember { mutableStateOf<Bitmap?>(null)}
+
+
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia(),
         onResult = { uri ->
             image2  = uriToBitmap(uri!!, context)
         }
     )
+
     val cameraLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.TakePicturePreview(),
         onResult = { photo ->
             image2 = photo
         }
     )
+
     val resources = context.resources
     val defaultImageBitmap = BitmapFactory.decodeResource(resources, R.drawable.camera).asImageBitmap()
+
     DisposableEffect(Unit) {
         val modelConditions = CustomModelDownloadConditions.Builder()
             .requireWifi()  // Also possible: .requireCharging() and .requireDeviceIdle()
             .build()
 
+
         FirebaseModelDownloader.getInstance()
-            .getModel("test1113", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND, modelConditions)
+            .getModel("ipf1", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND, modelConditions)
             .addOnSuccessListener { model: CustomModel? ->
                 val modelFile = model?.file
                 if (modelFile != null) {
                     interpreter = Interpreter(modelFile)
                 }
 
-                val bitmap1 = bitmapFromResource1
-                val bitmap2 = bitmapFromResource2
-                val bitmap3 = bitmapFromResource3
+                if (bitmapFromResource1 !=null && bitmapFromResource2 != null) {
+                    val input1 = preprocessImage(bitmapFromResource1)
+                    val input2 = preprocessImage(bitmapFromResource2)
+                    val input3 = preprocessImage(bitmapFromResource3)
 
-                if (bitmap1 != null && bitmap2 != null && bitmap3 != null) {
-                    val input1 = preprocessImage(bitmap1)
-                    val input2 = preprocessImage(bitmap2)
-                    val input3 = preprocessImage(bitmap3)
-
-                    val bufferSize = 2 * java.lang.Float.SIZE / java.lang.Byte.SIZE
+                    val bufferSize = 4 * java.lang.Float.SIZE / java.lang.Byte.SIZE
                     val modelOutput1 =
                         ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
                     val modelOutput2 =
@@ -106,45 +116,100 @@ fun DetectScreen(navController: NavController) {
                     interpreter?.run(input3, modelOutput3)
 
                     modelOutput1.rewind()
-                    val probabilities1 = modelOutput1.asFloatBuffer()
-                    try {
-                        val reader = BufferedReader(
-                            InputStreamReader(context.resources.openRawResource(R.raw.organic))
-                        )
-//                        val inputStream = context.resources.openRawResource("organic.txt")
-//                        val reader = BufferedReader(InputStreamReader(inputStream))
-                        for (i in 0 until probabilities1.capacity()) {
-                            val label: String = reader.readLine()
-                            val probability = probabilities1.get(i)
-                            println("$label: $probability")
-                            label1 = label
-                            prediction1 = probability.toString()
-                        }
-                    } catch (e: IOException) {
-                        // Handle the exception
-                        e.printStackTrace()
-                    }
+//                    val probabilities1 = modelOutput1.asFloatBuffer()
+                    // 소프트맥스 함수를 사용하여 확률 벡터 얻기
+                    val probabilities1 = FloatArray(4)
+                    softmax(modelOutput1.asFloatBuffer(), probabilities1)
+
+                    // 최대 확률을 가진 클래스 선택
+//                    val maxProbability = probabilities1.maxOrNull()
+//                    val predictedClassIndex = probabilities1.indexOf(maxProbability)
+                    val predictedClassIndex1 = findIndexOfMax(probabilities1)
+
+                    // 클래스 레이블 매핑
+                    val classLabels = arrayOf("S", "A", "B", "F")
+                    val predictedClassLabel1 = classLabels[predictedClassIndex1]
+                    prediction1 = predictedClassLabel1
+
+//                    try {
+//                        val reader = BufferedReader(
+//                            InputStreamReader(context.resources.openRawResource(R.raw.organic))
+//                        )
+////                        val inputStream = context.resources.openRawResource("organic.txt")
+////                        val reader = BufferedReader(InputStreamReader(inputStream))
+//                        for (i in 0 until probabilities1.capacity()) {
+//                            val label: String = reader.readLine()
+//                            val probability = probabilities1.get(i)
+//                            println("$label: $probability")
+//                            label1 = label
+//                            prediction1 = probability.toString()
+//                        }
+//                    } catch (e: IOException) {
+//                        // Handle the exception
+//                        e.printStackTrace()
+//                    }
 
                     modelOutput2.rewind()
-                    val probabilities2 = modelOutput2.asFloatBuffer()
+                    val numClasses = 4
+                    val probabilities2 = FloatArray(4)
+//                    softmax(modelOutput2.asFloatBuffer(), probabilities2)  <- try문 사용하면 softmax함수 사용할 필요없음
+
                     try {
                         val reader = BufferedReader(
                             InputStreamReader(context.resources.openRawResource(R.raw.organic))
                         )
-//                        val inputStream = context.resources.openRawResource("organic.txt")
-//                        val reader = BufferedReader(InputStreamReader(inputStream))
-                        for (i in 0 until probabilities2.capacity()) {
+
+                        // probabilities.capacity() 대신 numClasses를 사용하도록 변경
+                        for (i in 0 until numClasses) {
+                            // 클래스 레이블 읽어오기
                             val label: String = reader.readLine()
+
+                            // 확률 값 읽어오기
                             val probability = probabilities2.get(i)
+
+                            // 클래스 레이블과 확률 출력
                             println("$label: $probability")
-                            label2 = label
-                            prediction2 = probability.toString()
+
+                            // 각 클래스에 대한 확률을 배열에 저장
+                            probabilities2[i] = probability
                         }
                     } catch (e: IOException) {
-                        // Handle the exception
+                        // 예외 처리
                         e.printStackTrace()
                     }
 
+                    modelOutput3.rewind()
+                    val probabilities3 = FloatArray(4)
+                    softmax(modelOutput3.asFloatBuffer(), probabilities3)
+
+                    // 최대 확률을 가진 클래스 선택
+//                    val maxProbability = probabilities1.maxOrNull()
+//                    val predictedClassIndex = probabilities1.indexOf(maxProbability)
+                    val predictedClassIndex = findIndexOfMax(probabilities3)
+
+                    // 클래스 레이블 매핑
+                    val predictedClassLabel = classLabels[predictedClassIndex]
+                    prediction3 = predictedClassLabel
+
+//                    modelOutputNew.rewind()
+//                    val probabilitiesNew = modelOutputNew.asFloatBuffer()
+//                    try {
+//                        val reader = BufferedReader(
+//                            InputStreamReader(context.resources.openRawResource(R.raw.organic))
+//                        )
+////                        val inputStream = context.resources.openRawResource("organic.txt")
+////                        val reader = BufferedReader(InputStreamReader(inputStream))
+//                        for (i in 0 until probabilitiesNew.capacity()) {
+//                            val label: String = reader.readLine()
+//                            val probability = probabilitiesNew.get(i)
+//                            println("$label: $probability")
+//                            label2 = label
+//                            predictionNew = probability.toString()
+//                        }
+//                    } catch (e: IOException) {
+//                        // Handle the exception
+//                        e.printStackTrace()
+//                    }
 
                 }
             }
@@ -166,8 +231,9 @@ fun DetectScreen(navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(modifier = Modifier
-                .size(100.dp),
-                bitmap = image2?.asImageBitmap() ?: defaultImageBitmap, contentDescription = "image")
+                .size(300.dp),
+                bitmap = image2?.asImageBitmap() ?: defaultImageBitmap, contentDescription = "image"
+                )
 
             Row {
                 Button(
@@ -235,11 +301,16 @@ fun DetectScreen(navController: NavController) {
             Text(text = "2번째 $label2: $prediction2")
 
             Text(text = "3번째 $prediction3")
+
+
+            Button(onClick = { navController.navigate("evaluation") }) {
+                Text(text = "고객만족평가")
+            }
         }
     }
 }
 
-private fun preprocessImage(inputBitmap: Bitmap): ByteBuffer {
+fun preprocessImage(inputBitmap: Bitmap): ByteBuffer {
     val bitmap = Bitmap.createScaledBitmap(inputBitmap, 224, 224, true)
     val input = ByteBuffer.allocateDirect(224 * 224 * 3 * 4).order(ByteOrder.nativeOrder())
 
@@ -266,6 +337,7 @@ private fun preprocessImage(inputBitmap: Bitmap): ByteBuffer {
     }
     return input
 }
+
 fun bitmapToUri(context: Context, bitmap: Bitmap): Uri? {
     val contentValues = ContentValues().apply {
         put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
@@ -296,7 +368,66 @@ fun uriToBitmap(uri: Uri, context: Context): Bitmap? {
     }
 }
 
+//fun softmax(input: FloatArray, output: FloatArray) {
+//    if (input.size != output.size) {
+//        throw IllegalArgumentException("Input and output arrays must have the same size.")
+//    }
+//
+//    // Find the maximum value in the input array for numerical stability
+//    val maxInput = input.maxOrNull() ?: 0.0f
+//
+//    // Calculate the exponential of each element and the sum of exponentials
+//    var sumExp = 0.0f
+//    for (i in input.indices) {
+//        val expValue = exp(input[i] - maxInput)
+//        output[i] = expValue
+//        sumExp += expValue
+//    }
+//
+//    // Normalize by dividing each element by the sum of exponentials
+//    for (i in output.indices) {
+//        output[i] /= sumExp
+//    }
+//}
 
+fun softmax(input: FloatBuffer, output: FloatArray) {
+    if (input.capacity() != output.size) {
+        throw IllegalArgumentException("Input and output arrays must have the same size.")
+    }
+
+    // Find the maximum value in the input buffer for numerical stability
+    var maxInput = Float.NEGATIVE_INFINITY
+    for (i in 0 until input.capacity()) {
+        maxInput = maxInput.coerceAtLeast(input.get(i))
+    }
+
+    // Calculate the exponential of each element and the sum of exponentials
+    var sumExp = 0.0f
+    for (i in 0 until input.capacity()) {
+        val expValue = exp(input.get(i) - maxInput)
+        output[i] = expValue
+        sumExp += expValue
+    }
+
+    // Normalize by dividing each element by the sum of exponentials
+    for (i in 0 until input.capacity()) {
+        output[i] /= sumExp
+    }
+}
+
+fun findIndexOfMax(array: FloatArray): Int {
+    var maxIndex = 0
+    var maxValue = array[0]
+
+    for (i in 1 until array.size) {
+        if (array[i] > maxValue) {
+            maxIndex = i
+            maxValue = array[i]
+        }
+    }
+
+    return maxIndex
+}
 
 
 //
