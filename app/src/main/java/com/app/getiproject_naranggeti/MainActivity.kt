0cb9c776c23bb4301087e6aa4238c489255edb05
Jun <139526041+xax219@users.noreply.github.com>
2023-11-17 11:52:15 +0900

    sign in , sign up

--- app/src/main/java/com/app/getiproject_naranggeti/MainActivity.kt
+++ app/src/main/java/com/app/getiproject_naranggeti/MainActivity.kt
@@ -36,7 +36,7 @@ fun Navi() {
         ) {
 
             val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "start") {
+            NavHost(navController = navController, startDestination = "detect") {
                 composable("start") {
                     StartScreen(navController)
                 }
