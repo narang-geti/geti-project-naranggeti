7b9923b99c29947921ada3e1abb9984c0c2b64d7
Jun <139526041+xax219@users.noreply.github.com>
2023-11-18 10:18:06 +0900

    1118 1차 코드 합침

--- app/src/main/java/com/app/getiproject_naranggeti/Screen/CustomerEvaluation.kt
+++ app/src/main/java/com/app/getiproject_naranggeti/Screen/CustomerEvaluation.kt
@@ -2,7 +2,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
-fun CustomerEvaluation(navController: NavController) {
-
+fun CustomerEvaluation(navController: NavController){
 
 }
