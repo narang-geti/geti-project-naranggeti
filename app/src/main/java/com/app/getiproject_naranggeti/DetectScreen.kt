8ec8485aa2bfa746cb7c557df1ca8b96f7aa3cd9
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-11-18 09:49:34 +0900

    형준님 코드

--- app/src/main/java/com/app/getiproject_naranggeti/DetectScreen.kt
+++ app/src/main/java/com/app/getiproject_naranggeti/DetectScreen.kt
@@ -38,8 +38,6 @@ import androidx.core.graphics.green
 import androidx.core.graphics.red
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import java.io.FileNotFoundException
 import java.nio.FloatBuffer
